c6d388296009dd31985a1e10b9cda1c3bcfd37ad
Madison Helmstetter <ramon.madison@gmail.com>
2016-11-10 18:36:50 -0600

    added comments

--- httprequest.py
+++ httprequest.py
@@ -3,10 +3,13 @@ import json
 
 ########1.
 # send json that includes token and github to endpoint, using http post request
+
 url = "http://challenge.code2040.org/api/register"
 jsondata = {"token": "afa5d2bbbd8c9e0fb41eaf1cc1fd28fe", "github": "https://github.com/MadisonHe19/apiPractice"}
 header = {"content-type": "application/json"}
 
+# response object
 r = requests.post(url, json = jsondata, headers = header)
+
 
 ########2.
