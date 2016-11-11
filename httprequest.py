4a775fc4ac82eb4ec398af9541b9aba95ac40880
Madison Helmstetter <ramon.madison@gmail.com>
2016-11-10 20:56:20 -0600

    Reversed string from server and sent it back

--- httprequest.py
+++ httprequest.py
@@ -9,7 +9,28 @@ jsondata = {"token": "afa5d2bbbd8c9e0fb41eaf1cc1fd28fe", "github": "https://gith
 header = {"content-type": "application/json"}
 
 # response object
-r = requests.post(url, json = jsondata, headers = header)
+response = requests.post(url, json = jsondata, headers = header)
 
 
-########2.
+
+########2. send json to server and server should return string. Reverse string then send back
+url = "http://challenge.code2040.org/api/reverse"
+jsondata = { "token": "afa5d2bbbd8c9e0fb41eaf1cc1fd28fe" }
+header = {"content-type": "application/json"}
+
+#response object
+response = requests.post(url, json = jsondata, headers = header)
+
+#reversed string
+reversedString = response.text[::-1]
+
+url = "http://challenge.code2040.org/api/reverse/validate"
+jsondata = { "token": "afa5d2bbbd8c9e0fb41eaf1cc1fd28fe", "string" : reversedString}
+header = {"content-type": "application/json"}
+
+response = requests.post(url, json = jsondata, headers = header)
+
+safety = ["hello", "world", "you", "abra", "ces"]
+x = sorted(safety)
+for i in x:
+    print(i)
